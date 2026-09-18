3b7da562c170784447b2263bfb710cabdc51d534
corderox1 <cordero0james2@gmail.com>
2026-09-17 23:39:26 -0500

    Update update_online.py

--- update_online.py
+++ update_online.py
@@ -83,6 +83,15 @@ def make_leaderboard(history, start_date, stat):
 
         amount = latest[stat] - first[stat]
 
+        if latest["username"] in ["Corderox1", "Laika"]:
+    print(
+        "DEBUG:",
+        latest["username"],
+        "first =", first[stat],
+        "latest =", latest[stat],
+        "amount =", amount
+    )
+
         leaderboard.append({
             "playerId": player_id,
             "username": latest["username"],
