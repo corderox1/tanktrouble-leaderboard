725196a973b15e3eff467b234db3b996b15d1a8d
corderox1 <cordero0james2@gmail.com>
2026-09-18 00:06:19 -0500

    Update update_online.py

--- update_online.py
+++ update_online.py
@@ -84,12 +84,13 @@ def make_leaderboard(history, start_date, stat):
         amount = latest[stat] - first[stat]
 
         if player_id == "22710399":
-    print(
-        "CORDER DEBUG:",
-        "first =", first["date"], first[stat],
-        "latest =", latest["date"], latest[stat],
-        "amount =", amount
-    )
+            print(
+                "CORDER DEBUG:",
+                "first =", first["date"], first[stat],
+                "latest =", latest["date"], latest[stat],
+                "amount =", amount
+            )
+
 
         leaderboard.append({
             "playerId": player_id,
